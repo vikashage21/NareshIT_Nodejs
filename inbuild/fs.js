f4b5ce0a0b98de9da52e1b48ab74d81660899b1d
vikash sharma <vk220783@gmail.com>
2026-02-03 09:50:01 +0530

    fs all

--- inbuild/fs.js
+++ inbuild/fs.js
@@ -1,11 +1,44 @@
 
 let fs = require('fs')
 
-fs.writeFile('text.txt' , 'this is my data of node ' ,err =>{
+//fs.writeFile('text.txt' , 'this is my data of node ' ,err =>{
 
 
-if(err) throw err;
-	console.log('done');
+//if(err) throw err;
+//	console.log('done');
 
-} )
+//} )
+
+
+//fs.appendFile('text.txt' , 'code from developer' , err=> {
+//if (err) throw err;
+//	console.log('added the data in file');
+
+//}
+
+
+//)
+
+
+// fs.readFile('text.txt' , 'utf-8' , (error , data)=>{
+// 	if(error ) throw error ;
+// 	console.log(data)
+// }
+// )
+
+
+// async 
+
+
+const data = fs.readFileSync('text.txt',{encoding:'utf-8', flag:'r'})
+
+console.log(data)
+
+
+fs.unlink('text.txt', err=>{
+	if(err) throw err ;
+	console.log('file deleted')
+})
+
+
 
